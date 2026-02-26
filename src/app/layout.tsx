37b6f83547c3283d4b6a367d775e2cddd5929eb2
Sergey <aleksandrhepe@gmail.com>
2026-02-26 18:49:44 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import CookieBanner from "@/components/CookieBanner"; // ✅ добавили
 import { Kanit, Brygada_1918 } from "next/font/google";
 import { prisma } from "@/lib/prisma";
 import Head from "next/head";  // Подключаем Head
+import { useEffect } from "react"; // Импортируем useEffect для работы с клиентским кодом
 
 export const dynamic = "force-dynamic";
 
@@ -34,6 +35,35 @@ export default async function RootLayout({
   const text = marquee?.text ?? "СКИДКИ 20%";
   const speedSeconds = marquee?.speedSeconds ?? 100;
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.innerHTML = `
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) { return; }
+          }
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107021293', 'ym');
+
+        ym(107021293, 'init', {
+          ssr:true,
+          webvisor:true, 
+          clickmap:true, 
+          ecommerce:"dataLayer", 
+          referrer: document.referrer, 
+          url: location.href, 
+          accurateTrackBounce:true, 
+          trackLinks:true
+        });
+      `;
+      document.head.appendChild(script);
+    }
+  }, []); // Пустой массив зависимостей для выполнения только на клиенте
+
   return (
     <html lang="ru" className="h-full">
       <body
@@ -41,32 +71,6 @@ export default async function RootLayout({
       >
         {/* Яндекс.Метрика */}
         <Head>
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(m,e,t,r,i,k,a){
-                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                    m[i].l=1*new Date();
-                    for (var j = 0; j < document.scripts.length; j++) {
-                        if (document.scripts[j].src === r) { return; }
-                    }
-                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107021293', 'ym');
-    
-                ym(107021293, 'init', {
-                    ssr:true,
-                    webvisor:true, 
-                    clickmap:true, 
-                    ecommerce:"dataLayer", 
-                    referrer: document.referrer, 
-                    url: location.href, 
-                    accurateTrackBounce:true, 
-                    trackLinks:true
-                });
-              `,
-            }}
-          />
           <noscript>
             <div>
               <img src="https://mc.yandex.ru/watch/107021293" alt="" />
