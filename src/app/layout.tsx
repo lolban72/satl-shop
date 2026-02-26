64370ada5a19a07e57853736361037e8bdd4f676
Sergey <aleksandrhepe@gmail.com>
2026-02-26 18:39:49 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// src/app/layout.tsx или src/app/_document.js
+// src/app/layout.tsx
 import "../styles/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -41,30 +41,37 @@ export default async function RootLayout({
       >
         {/* Яндекс.Метрика */}
         <Head>
-          <script type="text/javascript">
-            {`
-              (function(m,e,t,r,i,k,a){
-                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                  m[i].l=1*new Date();
-                  for (var j = 0; j < document.scripts.length; j++) {
-                      if (document.scripts[j].src === r) { return; }
-                  }
-                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107021293', 'ym');
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){
+                    m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {
+                        if (document.scripts[j].src === r) { return; }
+                    }
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+                })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=107021293', 'ym');
     
-              ym(107021293, 'init', {
-                  ssr:true,
-                  webvisor:true, 
-                  clickmap:true, 
-                  ecommerce:"dataLayer", 
-                  referrer: document.referrer, 
-                  url: location.href, 
-                  accurateTrackBounce:true, 
-                  trackLinks:true
-              });
-            `}
-          </script>
-          <noscript><div><img src="https://mc.yandex.ru/watch/107021293" alt="" /></div></noscript>
+                ym(107021293, 'init', {
+                    ssr:true,
+                    webvisor:true, 
+                    clickmap:true, 
+                    ecommerce:"dataLayer", 
+                    referrer: document.referrer, 
+                    url: location.href, 
+                    accurateTrackBounce:true, 
+                    trackLinks:true
+                });
+              `,
+            }}
+          />
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/107021293" alt="" />
+            </div>
+          </noscript>
         </Head>
 
         {enabled && (
